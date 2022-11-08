171d5866873cd6bf6ddfc57264530a3c4856c1ba
zinefer <zinefer@gmail.com>
2022-11-08 10:34:40 -0700

    Bugfix: Adjust install script to accommodate release naming change

--- devops/scripts/install-hugo.sh
+++ devops/scripts/install-hugo.sh
@@ -17,11 +17,10 @@ echo "Searching for Hugo $VERSION"
 
 URL=`curl -s https://api.github.com/repos/gohugoio/hugo/releases \
       | jq -r --arg version $VERSION \
-          '.[] 
+          '[.[] 
           | select(.tag_name == $version) 
           | .assets[] 
-          | select(.browser_download_url 
-          | test("hugo_extended(.*)Linux-64bit.deb")) 
+          | select(.browser_download_url | test("hugo_extended(.*)Linux-64bit"))][0]
           | .browser_download_url'`
 
 echo "Found $URL"
@@ -30,6 +29,12 @@ INSTALLER=$(basename $URL)
 
 wget -q --show-progress -P /tmp $URL
 
-sudo dpkg -i /tmp/$INSTALLER
+# Around v0.102.3 hugo_extended(.*)Linux-64bit.deb was replaced with hugo_extended(.*)Linux-64bit.tar.gz
+if [[ $URL == *.tar.gz ]]; then
+  tar -C /tmp -xf /tmp/$INSTALLER
+  mv /tmp/hugo /usr/local/bin/hugo
+else
+  sudo dpkg -i /tmp/$INSTALLER
+fi
 
 rm /tmp/$INSTALLER
